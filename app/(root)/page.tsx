94112ac64e2c56ba940fad24efddc806b2cb7e3f
ssb <sb1664@gmail.com>
2025-03-20 14:37:57 +0000

    Add Carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,25 @@
+import ProductCarousel from '@/components/shared/Product/ProductCarousel';
 import ProductList from '@/components/shared/Product/ProductList';
-import { getLatestProducts } from '@/lib/actions/product.actions';
+import {
+  getFeaturedProducts,
+  getLatestProducts,
+} from '@/lib/actions/product.actions';
 // import sampleData from '@/db/sample-data';
 
-// export const metadata = {
-//   title: 'This Page',
-// };
-
 async function HomePage() {
-  const latestProducts = await getLatestProducts();
+  // const featuredProducts = await getFeaturedProducts();
+  // const latestProducts = await getLatestProducts();
+
+  const [featuredProducts, latestProducts] = await Promise.all([
+    getFeaturedProducts(),
+    getLatestProducts(),
+  ]);
 
   return (
     <>
+      {featuredProducts?.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList
         data={latestProducts}
         title='Newest Arrivals'
